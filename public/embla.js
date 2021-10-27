dd6ad93786a01218e875f7b589afc006c34adef4
Joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2021-10-27 23:46:09 +0100

    fix branch

--- public/embla.js
+++ public/embla.js
@@ -44,34 +44,3 @@ if (wrap !== null) {
   });
 }
 
-// const wrap = document.querySelector(".embla")
-// const viewPort = wrap.querySelector(".embla__viewport");
-// const prevBtn = wrap.querySelector(".embla__button--prev");
-// const nextBtn = wrap.querySelector(".embla__button--next");
-
-// const embla = EmblaCarousel(viewPort, {
-//   dragFree: true,
-//   containScroll: "trimSnaps",
-//   slidesToScroll: 4,
-// });
-
-// const setupPrevNextBtns = (prevBtn, nextBtn, embla) => {
-//   prevBtn.addEventListener("click", embla.scrollPrev, false);
-//   nextBtn.addEventListener("click", embla.scrollNext, false);
-// };
-// setupPrevNextBtns(prevBtn, nextBtn, embla);
-// const disablePrevNextBtns = (prevBtn, nextBtn, embla) => {
-//   return () => {
-//     if (embla.canScrollPrev()) prevBtn.removeAttribute("disabled");
-//     else prevBtn.setAttribute("disabled", "disabled");
-
-//     if (embla.canScrollNext()) nextBtn.removeAttribute("disabled");
-//     else nextBtn.setAttribute("disabled", "disabled");
-//   };
-// };
-
-// const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
-
-// embla.on("select", disablePrevAndNextBtns);
-// embla.on("init", disablePrevAndNextBtns);
-
