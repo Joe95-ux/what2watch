c4681f6ba87b26a642972d63fa33da2562bc6438
Joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2021-10-28 00:12:00 +0100

    changes to index.js

--- public/index.js
+++ public/index.js
@@ -22,10 +22,6 @@ const watchBanner = document.querySelector(".watch-banner");
 const watchProviders = document.querySelector(".watch-providers");
 const closeProviders = document.querySelector(".close-providers");
 
-// const playerWrapper = document.querySelector(".trailer-video");
-// const player = playerWrapper.getElementsByTagName("iframe");
-// const attribute = player[0].getAttribute("src");
-
 // navigation bar
 const navSlide = () => {
   const burger = document.querySelector(".burger");
