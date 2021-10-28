b8f4159292a9cd05ff6fd06d83f29542fcb7e29f
Joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2021-10-28 20:34:56 +0100

    fix app

--- app.js
+++ app.js
@@ -15,10 +15,7 @@ app.use(express.static( __dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
-app.use(forceDomain({
-  hostname: 'www.what2watch.net',
-  protocol: 'https'
-}));
+
 
 
 
