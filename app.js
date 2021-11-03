594d4286fcb3734972d224c51323cf19383c28c7
Joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2021-11-03 22:15:21 +0100

    fix ports

--- app.js
+++ app.js
@@ -261,8 +261,14 @@ app.get("/:page", async (req, res) => {
 app.use("/movie", movieRouter);
 app.use("/person", personRouter);
 app.use("/reviews", reviewRouter);
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server has started sucessfully");
 });
 
 
