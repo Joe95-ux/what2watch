26a949d97e98498692d9e91096844763dbe2b245
joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-06-27 23:06:58 +0100

    add mailchimp api for collecting emails

---
+++ public/validate.js
@@ -0,0 +1,141 @@
+const form = document.getElementById("form");
+const newsletterForm = document.getElementById("newsletter-form");
+const submitnlbtn = document.getElementById("submit");
+const thanks = document.querySelector(".thanks");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const cpassword = document.getElementById("cpassword");
+
+// show error message
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "input-field error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// show newsletter error
+function showNewsletterError(input, message){
+  const formControl = input.parentElement;
+  formControl.className = "news-letter-inner error";
+  const small = formControl.nextElementSibling;
+  small.innerText = message;
+
+}
+
+//show success message
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.classList.add("success");
+}
+
+// get field name
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+
+// check required fields
+
+function checkRequired(inputArr, e) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+      return e.preventDefault();
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// check news letter required fields
+
+function checkNLRequired(inputArr, e) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showNewsletterError(input, `${getFieldName(input)} is required`);
+      return e.preventDefault();
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+
+//check input length
+
+function checkLength(input, min, max, e) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+    return e.preventDefault();
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less ${max} characters`);
+    return e.preventDefault();
+  } else {
+    showSuccess(input);
+  }
+}
+
+//check that E-mail is valid
+function checkEmail(input, e) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "E-mail is not valid");
+    return e.preventDefault();
+  }
+}
+
+//check that newsletter E-mail is valid
+function checkNLEmail(input, e) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showNewsletterError(input, "E-mail is not valid");
+    return e.preventDefault();
+  }
+}
+
+// check password match
+
+function checkPasswordMatch(input1, input2, e) {
+  if (input1.value !== input2.value) {
+    showError(input2, "passwords do not match");
+    return e.preventDefault();
+  }
+}
+
+// if (form !== null) {
+//   form.addEventListener("submit", (e) => {
+//     if (cpassword === null) {
+//       checkRequired([email, password], e);
+//     } else {
+//       checkPasswordMatch(password, cpassword, e);
+//       checkRequired([email, password, cpassword], e);
+//     }
+//     checkLength(password, 8, 25, e);
+//     checkEmail(email, e);
+//   });
+// }
+
+function validateNewsletter(){
+  if(newsletterForm){
+    newsletterForm.addEventListener("submit", (e)=>{
+      checkNLRequired([email], e);
+      checkNLEmail(email, e);
+      submitnlbtn.innerText = "submiting...";
+    })
+  }
+}
+
+validateNewsletter();
--- routes/blog.js
+++ routes/blog.js
@@ -1,68 +1,75 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const {subcribeHandler} = require("../utils/mailchimp");
 
-
-router.get("/posts", async (req, res)=>{
-    const title = "blog posts";
-    res.render("blogHome", {title});
-})
-
-router.get("/category", async (req,res)=>{
-    const title = "category";
-    res.render("blogCategory", {title});
-
-    
-})
-
-router.get("/compose", async (req,res)=>{
-    const title = "compose";
-    res.render("compose", {title});
-
-    
-})
-
-router.get("/edit", async (req,res)=>{
-    const title = "edit post";
-    res.render("editblog", {title});
-
-    
-})
-
-router.get("/post", async (req,res)=>{
-    const title = "post";
-    res.render("post", {title});
-
-    
-})
-
-router.get("/login", async (req,res)=>{
-    const title = "Login";
-    res.render("login", {title});
-
-    
-})
-
-router.get("/register", async (req,res)=>{
-    const title = "register";
-    res.render("register", {title});  
-})
-
-router.get("/profile", async (req,res)=>{
-    const title = "Edit profile";
-    res.render("profile", {title});  
-})
-
-router.get("/dashboard", async (req,res)=>{
-    const title = "dashboard";
-    res.render("dashboard", {title});
-
-    
-})
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_SERVER_PREFIX
+});
 
 
 
+router.post("/posts", async (req, res) => {
+  const subscribingUser = {
+    firstName: "",
+    lastName: "",
+    email: req.body.subscriber
+  };
+  try{
+    await subcribeHandler(subscribingUser);
+    res.redirect("/blog/posts");
+  }catch(e){
+    console.log(e)
+  }
+  
+  
+});
 
+router.get("/posts", async (req, res) => {
+  const title = "blog posts";
+  res.render("blogHome.ejs", { title });
+});
 
+router.get("/category", async (req, res) => {
+  const title = "category";
+  res.render("blogCategory", { title });
+});
+
+router.get("/compose", async (req, res) => {
+  const title = "compose";
+  res.render("compose", { title });
+});
+
+router.get("/edit", async (req, res) => {
+  const title = "edit post";
+  res.render("editblog", { title });
+});
+
+router.get("/post", async (req, res) => {
+  const title = "post";
+  res.render("post", { title });
+});
+
+router.get("/login", async (req, res) => {
+  const title = "Login";
+  res.render("login", { title });
+});
+
+router.get("/register", async (req, res) => {
+  const title = "register";
+  res.render("register", { title });
+});
+
+router.get("/profile", async (req, res) => {
+  const title = "Edit profile";
+  res.render("profile", { title });
+});
+
+router.get("/dashboard", async (req, res) => {
+  const title = "dashboard";
+  res.render("dashboard", { title });
+});
 
 module.exports = router;
