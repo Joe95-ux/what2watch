9daa9eb0d5052052de42a8112a0255654bda8c68
joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-06-29 00:54:53 +0100

    add form validation for login and register forms

--- public/validate.js
+++ public/validate.js
@@ -1,32 +1,41 @@
-const form = document.getElementById("form");
+const form = document.getElementById("auth-form");
 const newsletterForm = document.getElementById("newsletter-form");
 const submitnlbtn = document.getElementById("submit");
 const thanks = document.querySelector(".thanks");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const secretToken = document.getElementById("secret-token");
 const cpassword = document.getElementById("cpassword");
 
 // show error message
 
 function showError(input, message) {
   const formControl = input.parentElement;
-  formControl.className = "input-field error";
-  const small = formControl.querySelector("small");
+  formControl.className = "actions-input error";
+  if(formControl.classList.contains("success")){
+    formControl.classList.remove("success");
+  }
+  const small = formControl.nextElementSibling;
   small.innerText = message;
 }
 
 // show newsletter error
-function showNewsletterError(input, message){
+function showNewsletterError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "news-letter-inner error";
+  if(formControl.classList.contains("success")){
+    formControl.classList.remove("success");
+  }
   const small = formControl.nextElementSibling;
   small.innerText = message;
-
 }
 
 //show success message
 function showSuccess(input) {
   const formControl = input.parentElement;
+  if(formControl.classList.contains("error")){
+    formControl.classList.remove("error");
+  }
   formControl.classList.add("success");
 }
 
@@ -36,12 +45,10 @@ function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-
-
 // check required fields
 
 function checkRequired(inputArr, e) {
-  inputArr.forEach(function (input) {
+  inputArr.forEach(function(input) {
     if (input.value.trim() === "") {
       showError(input, `${getFieldName(input)} is required`);
       return e.preventDefault();
@@ -54,7 +61,7 @@ function checkRequired(inputArr, e) {
 // check news letter required fields
 
 function checkNLRequired(inputArr, e) {
-  inputArr.forEach(function (input) {
+  inputArr.forEach(function(input) {
     if (input.value.trim() === "") {
       showNewsletterError(input, `${getFieldName(input)} is required`);
       return e.preventDefault();
@@ -63,7 +70,6 @@ function checkNLRequired(inputArr, e) {
     }
   });
 }
-
 
 //check input length
 
@@ -84,8 +90,7 @@ function checkLength(input, min, max, e) {
 
 //check that E-mail is valid
 function checkEmail(input, e) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
@@ -96,8 +101,7 @@ function checkEmail(input, e) {
 
 //check that newsletter E-mail is valid
 function checkNLEmail(input, e) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
     submitnlbtn.innerText = "submiting...";
@@ -116,25 +120,30 @@ function checkPasswordMatch(input1, input2, e) {
   }
 }
 
-// if (form !== null) {
-//   form.addEventListener("submit", (e) => {
-//     if (cpassword === null) {
-//       checkRequired([email, password], e);
-//     } else {
-//       checkPasswordMatch(password, cpassword, e);
-//       checkRequired([email, password, cpassword], e);
-//     }
-//     checkLength(password, 8, 25, e);
-//     checkEmail(email, e);
-//   });
-// }
+function validateAuth() {
+  if (form !== null) {
+    form.addEventListener("submit", e => {
+      if(secretToken != null){
+        checkRequired([email, password, secretToken], e);
+      }
+      if(cpassword != null){
+        checkRequired([email, password, cpassword], e);
+        checkPasswordMatch(password, cpassword, e);
+      }
+      checkLength(password, 8, 25, e);
+      checkEmail(email, e);
+    });
+  }
+}
+validateAuth();
 
-function validateNewsletter(){
-  if(newsletterForm){
-    newsletterForm.addEventListener("submit", (e)=>{
+
+function validateNewsletter() {
+  if (newsletterForm) {
+    newsletterForm.addEventListener("submit", e => {
       checkNLRequired([email], e);
       checkNLEmail(email, e);
-    })
+    });
   }
 }
 
