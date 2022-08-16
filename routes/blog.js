f2ff07a5b0c20086541ca225940142bd8d0b657c
joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-08-16 17:28:53 +0100

    fix bug in put request for a story

--- routes/blog.js
+++ routes/blog.js
@@ -368,7 +368,7 @@ router.put(
       }
 
       if (
-        story?.user?._id.equals(req.user._id) ||
+        story.user?._id.equals(req.user._id) ||
         req.user.privilege === "admin"
       ) {
         if (req.file) {
