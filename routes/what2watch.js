a4273638aac2c945c89416943cb252d2624f71cd
joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-10-25 23:05:37 +0100

    paginate watch recommendations

--- routes/what2watch.js
+++ routes/what2watch.js
@@ -22,69 +22,50 @@ async function getMovieGenre() {
 }
 
 async function getTvGenre() {
-    try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}&language=en-US`
-      );
-      const data = await response.json();
-      const genres = await data.genres;
-      return genres;
-    } catch (e) {
-      console.log(e);
-    }
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}&language=en-US`
+    );
+    const data = await response.json();
+    const genres = await data.genres;
+    return genres;
+  } catch (e) {
+    console.log(e);
   }
-
-function getTvAndMovies(all){
-    let list;
-    if(all.length){
-        list = all.filter(media=>media.media_type !== "person");
-        return list;
-    }
 }
 
+function getTvAndMovies(all) {
+  let list;
+  if (all.length) {
+    list = all.filter(media => media.media_type !== "person");
+    return list;
+  }
+}
 
-router.get("/what-to-watch", async(req, res)=>{
-    const title = "what to watch today";
-    let list = ["Top Picks", "watch Guide", "Popular"];
-    let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        const all = await data.results;
-        const media = getTvAndMovies(all);
-        const movieGenres = await getMovieGenre();
-        const tvGenres = await getTvGenre();
+router.get("/what-to-watch", async (req, res) => {
+  const title = "what to watch today";
+  let list = ["Top Picks", "watch Guide", "Popular"];
+  let page = 1;
+  if(req.query.page >=1){
+    page = parseInt(req.query.page);
+  }
+  let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&page=${page}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const all = await data.results;
+    const media = getTvAndMovies(all);
+    const movieGenres = await getMovieGenre();
+    const tvGenres = await getTvGenre();
+    const totalPages = await data.total_pages;
+    const totalResults = await data.total_results;
+    const page_num = await data.page;
 
-        res.render("what2watch", {media, movieGenres, tvGenres, list, title});
-
-
-    }catch(e){
-        console.log(e)
-    }
-})
-// watch recommendation page 2
-router.get("/what-to-watch/top-picks/", async(req, res)=>{
-    const title = "what to watch today";
-    let page = parseInt(req.query.page);
-    console.log(req.query, page);
-    
-    let list = ["Top Picks", "watch Guide", "Popular"];
-    let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&page=${page}`;
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        const all = await data.results;
-        const media = getTvAndMovies(all);
-        const movieGenres = await getMovieGenre();
-        const tvGenres = await getTvGenre();
-
-        res.render("what2watch", {media, movieGenres, tvGenres, list, title});
-
-
-    }catch(e){
-        console.log(e)
-    }
-})
+    res.render("what2watch", { media, movieGenres, tvGenres, list, title, totalPages, totalResults, page_num });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 
 module.exports = router;
